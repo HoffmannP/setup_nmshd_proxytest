11af9eb50b5794bf4270f1770302824388a70865
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2024-06-04 23:10:45 +0200

    Even more secure due to automated waiting

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/zsh
+set -e
 
-minikube kubectl -- apply -f Secret.yaml
-minikube kubectl -- create secret generic squidconf --from-file=debug.conf
+kubectl apply -f Secret.yaml
+kubectl create secret generic squidconf --from-file=debug.conf || echo "secret exists"
 
-minikube kubectl -- apply -f Deployment_Squid.yaml
-minikube kubectl -- apply -f Service_Squid.yaml
-minikube kubectl -- port-forward service/squid 3128:3128 &
+kubectl apply -f Deployment_Squid.yaml
+kubectl apply -f Service_Squid.yaml
+echo -n "Waiting for Deployment_Squid "; kubectl wait --timeout=1m -f Deployment_Squid.yaml --for=condition=available
+kubectl port-forward service/squid 3128:3128 &
 
-minikube kubectl -- apply -f Deployment_DB.yaml
-minikube kubectl -- apply -f Service_DB.yaml
+kubectl apply -f Deployment_DB.yaml
+kubectl apply -f Service_DB.yaml
 
-minikube kubectl -- apply -f Deployment_Connector.yaml
-minikube kubectl -- apply -f Service_Connector.yaml
+kubectl apply -f Deployment_Connector.yaml
+kubectl apply -f Service_Connector.yaml
+echo -n "Waiting for Deployment_Connector "; kubectl wait --timeout=1m -f Deployment_Connector.yaml --for=condition=available
+kubectl port-forward service/connector 8080:80 &
 
-minikube kubectl -- port-forward service/connector 8080:80 &
-
-minikube kubectl -- logs -f deployment/squid > direct_request_squid.log &
+kubectl logs -f deployment/squid > direct_request_squid.log &
 https_proxy=http://localhost:3128 curl -v https://nmshd-bkb.demo.meinbildungsraum.de/health > direct_request_curl.log
 kill %3
 
-minikube kubectl -- logs -f deployment/squid > connector_request_squid.log &
-minikube kubectl -- logs -f deployment/connector > connector_request_connector.log &
-curl -v http://localhost:8080/health > connector_request_curl.log
+kubectl logs -f deployment/squid > connector_request_squid.log &
+kubectl logs -f deployment/connector > connector_request_connector.log &
+curl -v http://localhost:8080/health > connector_request_curl.log 2>&1
 
 kill %4
 kill %3
