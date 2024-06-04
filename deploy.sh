9a9854d0abec3dd8f3600183c1e7e60d0675e122
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2024-06-05 00:22:33 +0200

    Simplify

--- deploy.sh
+++ deploy.sh
@@ -2,24 +2,28 @@
 set -e
 
 kubectl apply -f Secret.yaml
-kubectl create secret generic squidconf --from-file=debug.conf || echo "secret exists"
 
 kubectl apply -f Deployment_Squid.yaml
 kubectl apply -f Service_Squid.yaml
 echo -n "Waiting for Deployment_Squid "; kubectl wait --timeout=1m -f Deployment_Squid.yaml --for=condition=available
 kubectl port-forward service/squid 3128:3128 &
+sleep 2s
 
-kubectl apply -f Deployment_DB.yaml
-kubectl apply -f Service_DB.yaml
+kubectl apply -f Deployment_Mongo.yaml
+kubectl apply -f Service_Mongo.yaml
+echo -n "Waiting for Deployment_Mongo "; kubectl wait --timeout=1m -f Deployment_Mongo.yaml --for=condition=available
 
 kubectl apply -f Deployment_Connector.yaml
 kubectl apply -f Service_Connector.yaml
 echo -n "Waiting for Deployment_Connector "; kubectl wait --timeout=1m -f Deployment_Connector.yaml --for=condition=available
 kubectl port-forward service/connector 8080:80 &
+sleep 2s
+
+echo "Continue"; read
 
 kubectl logs -f deployment/squid > direct_request_squid.log &
-https_proxy=http://localhost:3128 curl -v https://nmshd-bkb.demo.meinbildungsraum.de/health > direct_request_curl.log
-kill %3
+https_proxy=http://localhost:3128 curl -v https://nmshd-bkb.demo.meinbildungsraum.de/health > direct_request_curl.log 2>&1
+kill %2
 
 kubectl logs -f deployment/squid > connector_request_squid.log &
 kubectl logs -f deployment/connector > connector_request_connector.log &
@@ -29,3 +33,8 @@ kill %4
 kill %3
 kill %2
 kill %1
+
+kubectl delete secret nmshd
+kubectl delete deployment squid mongo connector
+kubectl delete service squid mongo connector kubernetes
+killall kubectl
